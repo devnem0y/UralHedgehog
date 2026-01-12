2d3f9dd56bca72fb202efad377035d0e3d11c6f5
devnem0y <devnem0y@gmail.com>
2026-01-12 15:51:21 +0500

    Добавил WidgetSystem v1.0.1

---
+++ WidgetSystem/Editor/CreatePrefabContextMenu.cs
@@ -0,0 +1,44 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace UralHedgehog.UI
+{
+    public static class CreatePrefabContextMenu
+    {
+        private const string PREFAV_GUID = "13121a87220a15541ac2db931f0066bd";
+
+        [MenuItem("GameObject/Ural Hedgehog/UI/UIRoot", false, 0)]
+        private static void CreateCustomPrefab()
+        {
+            var path = AssetDatabase.GUIDToAssetPath(PREFAV_GUID);
+            if (string.IsNullOrEmpty(path))
+            {
+                Debug.LogError($"Prefab not found! GUID: {PREFAV_GUID}");
+                return;
+            }
+
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+            if (prefab == null)
+            {
+                Debug.LogError($"Failed to load prefab at path: {path}");
+                return;
+            }
+            
+            var instance = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            PlaceInScene(instance);
+        }
+
+        private static void PlaceInScene(GameObject instance)
+        {
+            if (Selection.activeTransform != null)
+            {
+                instance.transform.SetParent(Selection.activeTransform);
+            }
+
+            instance.transform.localPosition = Vector3.zero;
+            instance.name = instance.name.Replace("(Clone)", "UIRoot");
+            Undo.RegisterCreatedObjectUndo(instance, "Create Prefab");
+            Selection.activeObject = instance;
+        }
+    }
+}
---
+++ WidgetSystem/Runtime/Scripts/WidgetStorage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace UralHedgehog.UI
+{
+    [CreateAssetMenu(fileName = "WidgetStorage", menuName = "Ural Hedgehog/UI/Widget Storage", order = 0)]
+    public class WidgetStorage : ScriptableObject
+    {
+        [SerializeField] private MonoBehaviour[] _widgets;
+        public IEnumerable<MonoBehaviour> Widgets => _widgets;
+    }
+}
